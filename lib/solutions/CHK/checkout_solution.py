5df406607a9f7a031b0ef753bcd90fb4ca426360
sbx_user1051 <sbx_user1051@169.254.40.101>
2024-09-18 19:23:52 +0000

    Wed Sep 18 19:23:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,20 @@
 import collections
 
+def get_price(item, frequency):
+    match item:
+        case "A":
+            return 50
+        case _:
+            return 0
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    price_list = {
-        "A": {re}
-    }
     result = collections.Counter(skus)
+    total_price = 0
+    for sku, frequency in result.items():
+        total_price += get_price(sku, frequency)
     
+    return total_price
+
 
