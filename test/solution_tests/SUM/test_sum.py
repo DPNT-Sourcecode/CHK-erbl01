a9525e400bb60d3b41fcf574624a4bb6c9397c55
sbx_user1051 <sbx_user1051@169.254.22.49>
2024-09-18 18:42:01 +0000

    Wed Sep 18 18:42:01 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,6 +5,8 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
-    def test_x_is_positive(self):
-        assert sum_solution(-1, 2) == 
+    def test_x_cant_be_negative(self):
+        with pytest.raises(ValueError):
+            sum_solution(-1, 2)
+
 
