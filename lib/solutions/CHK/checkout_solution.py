3f97e098c6fe8263fb362620a5a3b0170aab700d
sbx_user1051 <sbx_user1051@169.254.40.101>
2024-09-18 19:29:52 +0000

    Wed Sep 18 19:29:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,10 @@ import collections
 def get_price(item, frequency):
     match item:
         case "A":
+            value = 50
+            special_offer = 130
+            if frequency > 3:
+                frequency % 3
             return 50
         case _:
             return 0
@@ -19,5 +23,6 @@ def checkout(skus):
         total_price += get_price(sku, frequency)
     
     return total_price
+
 
 
