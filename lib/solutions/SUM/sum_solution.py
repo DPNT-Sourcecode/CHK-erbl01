c910abc749f45f8aa94fe0d719b401e78f423bee
sbx_user1051 <sbx_user1051@169.254.40.101>
2024-09-18 18:53:10 +0000

    Wed Sep 18 18:53:10 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int):
     if x < 0 or x > 100:
-        raise ValueError
+        raise ValueError("Number has to be between 0 and 100")
+
+    if y < 0 or y > 100:
+        raise ValueError("Number has to be between 0 and 100")
+
     return x + y
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,7 +3,7 @@ from solutions.SUM import sum_solution
 
 
 class TestSum():
-    def test_sum(self):
+    def test_sum_positive_numbers(self):
         assert sum_solution.compute(1, 2) == 3
 
     def test_x_cant_be_negative(self):
@@ -13,5 +13,19 @@ class TestSum():
     def test_x_cant_be_more_than_100(self):
         with pytest.raises(ValueError):
             sum_solution.compute(101, 2)
-            
+
+    def test_y_cant_be_negative(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(1, -1)
+
+    def test_y_cant_be_more_than_100(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(1, 101)
+
+    def test_sum_negative_numbers(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(-1, -1)
+
+
+
 
