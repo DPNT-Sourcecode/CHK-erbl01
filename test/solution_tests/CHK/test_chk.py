96585b37d0322567966a0a2d673c70bf93a407ae
sbx_user1051 <sbx_user1051@169.254.132.61>
2024-09-19 17:28:51 +0000

    Thu Sep 19 17:28:51 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -11,9 +11,10 @@ class TestCHK():
         assert checkout_solution.checkout(sku*offer["units_required"]) == offer["price"]
   
   def test_buy_x_get_y_free(self):
-    for sku, offers in checkout_solution.PRICES.items():
-      for offer in offers:
+    for sku, offer in checkout_solution.BUY_X_GET_Y_FREE.items():
+      assert checkout_solution.checkout(sku*offer["units_required"]+offer["item_free"]) == checkout_solution.PRICES[sku][-1]["price"]*offer["units_required"]
 
   
+
 
 
