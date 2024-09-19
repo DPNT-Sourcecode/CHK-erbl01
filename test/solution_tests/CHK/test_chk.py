6f6e00654fc4a2fe16774d0cfb9021b250d85828
sbx_user1051 <sbx_user1051@169.254.132.61>
2024-09-19 17:31:50 +0000

    Thu Sep 19 17:31:50 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -9,12 +9,13 @@ class TestCHK():
     for sku, offers in checkout_solution.PRICES.items():
       for offer in offers:
         assert checkout_solution.checkout(sku*offer["units_required"]) == offer["price"]
-  
+
   def test_buy_x_get_y_free(self):
     for sku, offer in checkout_solution.BUY_X_GET_Y_FREE.items():
       assert checkout_solution.checkout(sku*offer["units_required"]+offer["item_free"]) == checkout_solution.PRICES[sku][-1]["price"]*offer["units_required"]
 
   
+
 
 
 
